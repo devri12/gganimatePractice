e9090d158201ef4136ebfa38e290f48c906ba90f
Devri Adams <devri.adams@duke.edu>
2017-06-14 15:42:13 -0400

    shiny gganimate

--- server.R
+++ server.R
@@ -1,3 +1,4 @@
+library(shiny)
 library(gganimate)
 library(ggplot2)
 library(readr)
@@ -6,13 +7,32 @@ library(animation)
 #load in the data from the download
 precip_stream_data <- read_rds("D:/Duke/Work(Environ)/Programming/AcidRainStory/DataCleaning/precip_stream_data.rds")
 
-small_data_practice <- precip_stream_data[c(1:20, 234:253, 467:486),]
+#make subsection of precip_stream_data to make quick practice gganimates
+small_data_practice <- precip_stream_data[c(1:20, 234:253, 467:486, 56824:56843, 57052:57071, 57281:57300),]
 
+#make the ImageMagick thing able to convert to produce animation... tbh got this from online
 Sys.setenv(PATH = paste("C:/Users/Devri/Downloads/ImageMagick-7.0.6-0-portable-Q16-x86",
                         Sys.getenv("PATH"), sep = ";"))
 
+function(input, output){
+
+output$g <- renderImage({
+  #online code... "a temp file to save the output, will be removed later by renderImage
+  outfile <- tempfile(fileext = '.gif')
+  #make animation
 g <- ggplot(small_data_practice,
-            aes(x = solute, y = concentration_ueq, size = MW, color = source, frame = date))+
+            aes(x = source, y = concentration_ueq, size = MW, color = solute, frame = date))+
   geom_point()
 
-gganimate(g)
+gganimate(g, "outfile.gif")
+
+#not sure why but they say to return a list containing the filename
+list(src = "outfile.gif",
+     contentType = 'image/gif'
+     #width = 400,
+     #height = 300
+     #alt = "This is alternate text"
+     )}, deleteFile = TRUE)
+
+
+}
---
+++ ui.R
@@ -0,0 +1,7 @@
+library(shiny)
+library(gganimate)
+
+fluidPage(
+  #google said to use imageOutput for gganimate so I did
+  imageOutput("g")
+)
